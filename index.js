87d260200d305d8f30867ef0500ab463201a1e57
Sam Parsons <samparsons269@gmail.com>
2021-05-20 22:59:43 -0400

    removed reference error throw in API detection, replaced with console warn

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ export default class Sanitize extends React.PureComponent {
   constructor(props) {
     super(props);
     // check if Sanitizer API exists in window
+    this.failedMount = false;
     if (!('Sanitizer' in window)) {
-      return new ReferenceError('Sanitizer API does not exist in this browser');
+      console.warn('Sanitizer API does not exist in this browser');
+      this.failedMount = true;
     }
     this.Sanitizer = new window.Sanitizer();
   }
 
   render() {
-    return !this.props.children.length
-      ? this.props.children
-      : this.props.children.map((child) => {
-          const newHandler = function (e) {
-            e.target.sanitizedValue = this.Sanitizer.sanitizeToString(
-              e.target.value
+    return this.failedMount
+      ? !this.props.children.length
+        ? this.props.children
+        : this.props.children.map((child) => {
+            const newHandler = function (e) {
+              e.target.sanitizedValue = this.Sanitizer.sanitizeToString(
+                e.target.value
+              );
+              child.props.onChange(e);
+            }.bind(this);
+            const newProps = Object.assign(
+              { ...child.props },
+              { onChange: newHandler }
             );
-            child.props.onChange(e);
-          }.bind(this);
-          const newProps = Object.assign(
-            { ...child.props },
-            { onChange: newHandler }
-          );
-          const newChild = React.createElement(child.type, newProps);
-          return newChild;
-        });
+            const newChild = React.createElement(child.type, newProps);
+            return newChild;
+          })
+      : this.props.children;
   }
 }
